c949b7a7d030df3fcee78d6797f06246e8330488
aghilazghi <amineteclai@gmail.com>
2020-11-30 21:42:55 -0500

    Add built in pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  rate: number;
+
+  onNameChange(value: string){
+    this.name = value;
+  }
+
+  onDateChange(value: string){
+    this.date = value;
+  }
+
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+
+  onRateChange(value: string){
+    this.rate = parseFloat(value);
+  }
+}
